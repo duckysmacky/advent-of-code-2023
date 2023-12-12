347ed21c52ea750a4d42ed7319f6ca540a161429
Ducky <kriperoka@gmail.com>
2023-12-12 15:18:17 +0300

    day 12 wip

--- Day12/one.java
+++ Day12/one.java
@@ -1,9 +1,13 @@
-
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class one {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         
-
+        FileReader input = new FileReader("input.txt");
+        
+            
 
     }
 }
